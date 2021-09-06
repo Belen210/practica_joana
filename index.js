368d3579b2f815f0a4783a7b0ed842737c281116
Belen210 <89800978+Belen210@users.noreply.github.com>
2021-09-06 12:07:05 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+fuction getIMC(){
+  var peso=parsefloar(docoment.getElemt byid("peso".valuie);
+  var altura=parsefloat(document,getElent By id("altura").value);
+  var IMC=peso/(altura * altura)
+  document.getElement By id(2IMC").imcHTML=imc;
+{
